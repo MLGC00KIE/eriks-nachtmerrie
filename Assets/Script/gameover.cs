1238a49f643b955bb572e920921a5546183ae9b2
Eigenaar <toonschaap3@gmail.com>
2018-05-09 11:02:01 +0200

    Game over

---
+++ Assets/Script/gameover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameover : MonoBehaviour {
+	void OnCollisionEnter(Collision collision)
+{
+	if (collision.gameObject.tag == "Player")
+	{
+		SceneManager.LoadScene ("controls");
+		
+	}
+}
+}
